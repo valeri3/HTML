cf6be02c8e599cafaaa5e50c36bb80cfc3582c24
Lera <valeriproh@gmail.com>
2023-12-06 13:24:20 +0100

    add HW

--- JS/HW 05.12/js/main.js
+++ JS/HW 05.12/js/main.js
@@ -1,32 +1,11 @@
-let myCar = new Car("Toyota", "Camry", 2020, 80);
-console.log("Задание 1");
-console.log(myCar.displayInfo());
-console.log(`Необходимое время для преодоления 600 км: ${myCar.calculateTravelTime(600)} ч.`);
-
-let frac1 = new Fraction(1, 2);
-let frac2 = new Fraction(3, 4);
-console.log("Задание 2");
-let sum = frac1.add(frac2);
-console.log(`Сумма: ${sum.a}/${sum.b}`);
-
-let diff = frac1.subtract(frac2);
-console.log(`Разность: ${diff.a}/${diff.b}`);
-
-let mult = frac1.multiply(frac2);
-console.log(`Произведение: ${mult.a}/${mult.b}`);
-
-let div = frac1.divide(frac2);
-console.log(`Частное: ${div.a}/${div.b}`);
-
-console.log("Задание 3");
-let currentTime = new Time(20, 30, 45);
-console.log("Текущее время:", currentTime.displayTime());
-
-currentTime.addSec(40);
-console.log("Добавляем 40 секунд:", currentTime.displayTime());
-
-currentTime.addMin(23);
-console.log("Добавляем 23 минуты:", currentTime.displayTime());
-
-currentTime.addHr(9);
-console.log("Добавляем 9 часов:", currentTime.displayTime());
+Task1();
+b();
+c('error');
+createHeaders(5);
+console.log("Task 4:");
+console.log(checkPass("Web"));
+console.log(checkPass("C++"));
+console.log("Task 5:");
+console.log(signNum(-5));
+console.log("Task 6:");
+console.log(days(3));
